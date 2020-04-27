a8befbed84a18b259d6ce1ece93d3bb3977f0119
Olivia <olivia.wright101@gmail.com>
2020-04-27 20:18:11 +0100

    Removed commented out code and react images

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -93,47 +93,4 @@ const Pokedex = () => {
   )
 }
 
-/* <PokemonNumberSearch pokemonList={pokemonList} onSelectPokemon={setSelectedPokemonUrl}/> */
-/* <PokemonNameSearch pokemonList={pokemonList} onSelectPokemon={setSelectedPokemonUrl}/> */
-
 export default Pokedex;
-
-{/* <section className="pokedex-box">
-<div className="main-pokedex-body">
-    <div className="left-gray-circle">
-        <div className="left-darkgrey-circle"></div>
-        <div className="left-green-circle"></div>
-    </div>
-    <div className="left-red-vertical-bar"></div>
-    <div className="hinge-bar"></div>
-    <div className="hinge-bar-indent"></div>
-    <div className="info-box-border-top">
-        <div className="info-box-border-double"></div>
-        <div className="top-darkgrey-box"></div>
-        <section className="info-box">
-          <SearchExample pokemonList={pokemonList} onSelectPokemon={setSelectedPokemonUrl} />
-          <SelectedPokemonImage pokemon={selectedPokemon}/>
-        </section>
-    </div>
-    <div className="info-box-border-bottom">
-        <div className="info-box-border-double"></div>
-        <div className="bottom-darkgrey-box"></div>
-        
-        <section className="info-box">
-          <SelectedPokemonDetail pokemon={selectedPokemon}/>
-        </section>
-    </div>
-    <div className="d-pad">
-        <div className="button b-up"></div>
-        <div className="button b-right"></div>
-        <div className="button b-down"></div>
-        <div className="button b-left"></div>
-        <div className="button b-middle"></div>
-    </div>
-</div>
-<div className="right-control-pad">
-
-    <div className="cp-gray-circle"></div>
-    <div className="cp-gray-square"></div>
-</div>
-</section>  */}
